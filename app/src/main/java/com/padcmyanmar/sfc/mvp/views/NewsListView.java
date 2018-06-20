c271c0968b0d639aafd454f558aaec585fdbab56
htoo <htooaungnaing1102@gmail.com>
2018-06-21 00:52:43 +0630

    initial commit

---
+++ app/src/main/java/com/padcmyanmar/sfc/mvp/views/NewsListView.java
@@ -0,0 +1,14 @@
+package com.padcmyanmar.sfc.mvp.views;
+
+import com.padcmyanmar.sfc.data.vo.NewsVO;
+
+import java.util.List;
+
+public interface NewsListView extends BaseView{
+    void displayNewsList(List<NewsVO> newsList);
+
+    void displayErrorMsg(String errMsg);
+
+    void launchNewsDetailsScreen(String newsId);
+
+}
