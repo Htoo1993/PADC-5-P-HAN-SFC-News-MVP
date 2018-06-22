d019d2e00a905b396a3daacefff42d89316b6186
htoo <htooaungnaing1102@gmail.com>
2018-06-22 06:42:41 +0630

    list data bind

--- app/src/main/java/com/padcmyanmar/sfc/activities/NewsListActivityMVP.java
+++ app/src/main/java/com/padcmyanmar/sfc/activities/NewsListActivityMVP.java
@@ -1,22 +1,37 @@
 package com.padcmyanmar.sfc.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.padcmyanmar.sfc.R;
 import com.padcmyanmar.sfc.adapters.NewsAdapterMVP;
 import com.padcmyanmar.sfc.components.SmartRecyclerView;
+import com.padcmyanmar.sfc.data.vo.NewsVO;
+import com.padcmyanmar.sfc.mvp.presenters.NewsListPresenter;
+import com.padcmyanmar.sfc.mvp.views.NewsListView;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class NewsListActivityMVP extends BaseActivity {
+public class NewsListActivityMVP extends BaseActivity
+            implements NewsListView{
 
     @BindView(R.id.rv_news)
-    SmartRecyclerView rvNews;
+    RecyclerView rvNews;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     private NewsAdapterMVP mNewsAdapterMVP;
+
+    private NewsListPresenter mNewsListPresenter;
 
 
     @Override
@@ -25,8 +40,59 @@ public class NewsListActivityMVP extends BaseActivity {
         setContentView(R.layout.activity_news_list_mvp);
         ButterKnife.bind(this,this);
 
+        setSupportActionBar(toolbar);
+
+        mNewsListPresenter = new NewsListPresenter(this);
+        mNewsListPresenter.onCreate();
+
         rvNews.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
         mNewsAdapterMVP = new NewsAdapterMVP(getApplicationContext());
         rvNews.setAdapter(mNewsAdapterMVP);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mNewsListPresenter.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mNewsListPresenter.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mNewsListPresenter.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mNewsListPresenter.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mNewsListPresenter.onDestroy();
+    }
+
+    @Override
+    public void displayNewsList(List<NewsVO> newsList) {
+        mNewsAdapterMVP.appendNewData(newsList);
+    }
+
+    @Override
+    public void displayErrorMsg(String errMsg) {
+        Snackbar.make(rvNews, errMsg, Snackbar.LENGTH_INDEFINITE).show();
+    }
+
+    @Override
+    public void launchNewsDetailsScreen(String newsId) {
+        Intent intent = NewsDetailsActivity.newIntent(getApplicationContext(),newsId);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/padcmyanmar/sfc/viewholders/NewsViewHolderMVP.java
+++ app/src/main/java/com/padcmyanmar/sfc/viewholders/NewsViewHolderMVP.java
@@ -1,10 +1,33 @@
 package com.padcmyanmar.sfc.viewholders;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.padcmyanmar.sfc.R;
 import com.padcmyanmar.sfc.data.vo.NewsVO;
 
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+
 public class NewsViewHolderMVP extends BaseViewHolder<NewsVO> {
+
+    @BindView(R.id.iv_news_image)
+    ImageView ivNewsImage;
+
+    @BindView(R.id.iv_publication_logo)
+    ImageView ivPublicationLogo;
+
+    @BindView(R.id.tv_publication_name)
+    TextView tvPublicationName;
+
+    @BindView(R.id.tv_brief_news)
+    TextView tvBriefNews;
+
+    @BindView(R.id.tv_posted_date)
+    TextView tvPostedDate;
 
     public NewsViewHolderMVP(View itemView) {
         super(itemView);
@@ -13,6 +36,21 @@ public class NewsViewHolderMVP extends BaseViewHolder<NewsVO> {
     @Override
     public void setData(NewsVO data) {
 
+        tvBriefNews.setText(data.getDetails());
+        tvPublicationName.setText(data.getPublication().getTitle());
+        tvPostedDate.setText(data.getPostedDate());
+
+        if (!data.getImages().isEmpty()){
+            Glide.with(ivNewsImage.getContext())
+                    .load(data.getImages().get(0))
+                    .into(ivNewsImage);
+        } else {
+            ivNewsImage.setVisibility(View.GONE);
+        }
+
+        Glide.with(ivPublicationLogo.getContext())
+                .load(data.getPublication().getLogo())
+                .into(ivPublicationLogo);
     }
 
     @Override
